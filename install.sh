8bc3654036dbfbd629da9afd59fa4c9ac614da3e
Viorel ZP <viorel@reea.net>
2022-09-20 12:30:24 +0300

    fix file copy

--- install.sh
+++ install.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-find . -mindepth 1 -maxdepth 1 -type f -name 'bash*' | while read -r BFILE; do
+find . -mindepth 1 -maxdepth 1 -type f -name '.bash*' | while read -r BFILE; do
   F2REPLACE=$(basename "${BFILE}")
-  F2REPLACE=".${F2REPLACE}"
   # echo "$HOME/${F2REPLACE} will be replaced!"
   if [ -f "$HOME/${F2REPLACE}" ]; then
     echo "$HOME/${F2REPLACE} will be replaced!"
   fi
+  cp "${F2REPLACE}" "$HOME/${F2REPLACE}"
 done
-
